1b46e3c0b004a4480b9316fc8d2e5d24aaa79ce6
arthurguerra <arthur_gb1996@hotmail.com>
2021-09-20 20:03:46 -0300

    Developed unit tests for create person

---
+++ src/test/java/one/digital/innovation/personapi/service/PersonServiceTest.java
@@ -0,0 +1,48 @@
+package one.digital.innovation.personapi.service;
+
+import one.digital.innovation.personapi.dto.request.PersonDTO;
+import one.digital.innovation.personapi.dto.response.MessageResponseDTO;
+import one.digital.innovation.personapi.entity.Person;
+import one.digital.innovation.personapi.repository.PersonRepository;
+import one.digital.innovation.personapi.utils.PersonUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static one.digital.innovation.personapi.utils.PersonUtils.createFakeDTO;
+import static one.digital.innovation.personapi.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSaveMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedSuccessMessage(expectedSavedPerson.getId());
+        MessageResponseDTO successMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    private MessageResponseDTO createExpectedSuccessMessage(Long savedPersonId) {
+        return MessageResponseDTO.builder()
+                .message("Created person with ID: " + savedPersonId)
+                .build();
+    }
+}
